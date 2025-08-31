e35f757afc8eb0052936d45c4b0c3247cd987def
a.beresov <aleksandr.beresov@gmail.com>
2025-08-31 13:07:49 -0300

    getting options

--- EnviromentStartupWinProcedure.bat
+++ EnviromentStartupWinProcedure.bat
@@ -18,31 +18,35 @@ set "SUCCESS_COUNT=0"
 
 :: === PROCESS CONFIGURATION ===
 for /f "tokens=1,2 delims==" %%A in (%CONFIG_FILE%) do (
-    :: Validate if the executable exists
-    if not exist "%%B" (
-        echo [ERROR] %%A executable not found at "%%B".
-        echo Please check the path in the configuration file.
-        set "RUN_%%A=0"
-    ) else (
-        set "RUN_%%A=1"
-    )
-
-    :: Check if the program is already running
-    if "!RUN_%%A!"=="1" (
-        echo Checking if %%A is already running...
-        tasklist | find /i "%%~nB.exe" >nul
-        if !errorlevel!==0 (
-            echo %%A is already running.
+    for /f "tokens=1,2 delims=|" %%X in ("%%B") do (
+        set "EXE_PATH=%%X"
+        set "EXE_PARAMS=%%Y"
+        REM Validate if the executable exists
+        if not exist "%%X" (
+            echo [ERROR] %%A executable not found at "%%X".
+            echo Please check the path in the configuration file.
+            set "RUN_%%A=0"
         ) else (
-            echo Starting %%A...
-            start "" "%%B"
-            ping 127.0.0.1 -n 1 -w 500 >nul
-            tasklist | find /i "%%~nB.exe" >nul
+            set "RUN_%%A=1"
+        )
+echo %%X
+echo %%Y
+        if "!RUN_%%A!"=="1" (
+            echo Checking if %%A is already running...
+            tasklist | find /i "%%~nX.exe" >nul
             if !errorlevel!==0 (
-                echo %%A launched successfully.
-                set /a SUCCESS_COUNT+=1
+                echo %%A is already running.
             ) else (
-                echo [ERROR] Failed to launch %%A.
+                echo Starting %%A...
+                start "" "%%X" %%Y
+                ping 127.0.0.1 -n 1 -w 500 >nul
+                tasklist | find /i "%%~nX.exe" >nul
+                if !errorlevel!==0 (
+                    echo %%A launched successfully.
+                    set /a SUCCESS_COUNT+=1
+                ) else (
+                    echo [ERROR] Failed to launch %%A.
+                )
             )
         )
     )
